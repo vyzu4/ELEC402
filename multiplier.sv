ee94ec0379bbda0b4317ed82e60a072a9a638c9b
vyzu4 <99065703+vyzu4@users.noreply.github.com>
2025-11-18 03:00:24 -0800

    Update multiplier.sv

--- multiplier.sv
+++ multiplier.sv
@@ -57,14 +57,14 @@ module multiplier #(
 
         if (rst) begin
             state = IDLE;
-            // // initialize all i/o
-            // EN_writeMem = 1'b0;
-            // writeMem_addr = 6'b0;
-            // writeMem_val = 16'b0;
-            // RDY_mult = 1'b0;
-            // VALID_memVal = 1'b0;
-            // EN_readMem = 1'b0;
-            // readMem_addr = 6'b0;
+            // initialize all i/o
+            EN_writeMem = 1'b0;
+            writeMem_addr = 6'b0;
+            writeMem_val = 16'b0;
+            RDY_mult = 1'b0;
+            VALID_memVal = 1'b0;
+            EN_readMem = 1'b0;
+            readMem_addr = 6'b0;
         end
         else
             // transition to next state
